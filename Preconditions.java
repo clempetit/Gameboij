402af1381750a0fa37452e5b2763b25efb9ac20a
yanisepfl <33152568+yanisepfl@users.noreply.github.com>
2018-02-25 19:44:24 +0100

    Update Preconditions.java

--- Preconditions.java
+++ Preconditions.java
@@ -5,14 +5,29 @@
 
 package ch.epfl.gameboj;
 
+/**
+ * 
+ * @author Yanis Berkani (
+ *@author Clément Petit (
+ */
 public interface Preconditions {
     
+    /**
+     * throws the exception IllegalArgumentException if its parameter  is false
+     * do nothing otherwise
+     * @param b the condition
+     */
     public static void checkArgument(boolean b) {
         if (!b) {
             throw new IllegalArgumentException();
         }
     }
     
+    /**
+     * 
+     * @param v
+     * @return
+     */
     public static int checkBits8(int v) {
         if (v<0 || v>0xFF) {
             throw new IllegalArgumentException();
@@ -20,6 +35,11 @@ public interface Preconditions {
         return v;
     }
     
+    /**
+     * 
+     * @param v
+     * @return
+     */
     public static int checkBits16(int v) {
         if (v<0 || v>0xFFFF) {
             throw new IllegalArgumentException();
