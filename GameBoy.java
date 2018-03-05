06917641e2e3d0b8987248db9b812acd981d2804
clempetit <33152614+clempetit@users.noreply.github.com>
2018-03-05 10:51:37 +0100

    Add files via upload

--- GameBoy.java
+++ GameBoy.java
@@ -1,6 +1,6 @@
 /**
- *  @autor Clément Petit (282626)
- *  @autor Yanis Berkani (271348)
+ *	@author Clément Petit (282626)
+ *	@author Yanis Berkani (271348)
  */
 
 package ch.epfl.gameboj;
@@ -12,15 +12,16 @@ public class GameBoy {
     
     private Bus bus;
     private Ram workRam;
-    private RamController ramController1;
-    private RamController ramController2;
+    private RamController workRamCtrl;
+    private RamController echoRamCtrl;
+    
     public GameBoy(Object cartridge){
+        bus = new Bus();
         workRam = new Ram(AddressMap.WORK_RAM_SIZE);
-        ramController1 = new RamController(workRam, AddressMap.WORK_RAM_START, AddressMap.WORK_RAM_END);
-        ramController2 = new RamController(workRam, AddressMap.ECHO_RAM_START, AddressMap.ECHO_RAM_END);
-        
-        bus.attach(ramController1);
-        bus.attach(ramController2);
+        workRamCtrl = new RamController(workRam, AddressMap.WORK_RAM_START, AddressMap.WORK_RAM_END);
+        echoRamCtrl = new RamController(workRam, AddressMap.ECHO_RAM_START, AddressMap.ECHO_RAM_END);
+        bus.attach(workRamCtrl);
+        bus.attach(echoRamCtrl);
     }
     
     public Bus bus() {
