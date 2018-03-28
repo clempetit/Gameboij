b777ee724f1dc14413f41d7c2856b6700ea83b9f
yanisepfl <33152568+yanisepfl@users.noreply.github.com>
2018-03-28 02:31:59 +0200

    Update Clocked.java

--- Clocked.java
+++ Clocked.java
@@ -6,6 +6,10 @@
 package ch.epfl.gameboj.component;
 
 public interface Clocked {
-    
+    /**
+     * asks to the component to evolve by executing all the 
+     * operations that it has to execute during the given cycle
+     * @param cycle the cycle
+     */
     public abstract void cycle(long cycle);
 }
