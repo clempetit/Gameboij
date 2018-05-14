e32c19a040706befdd3c7ee09d53858480c93a89
clempetit <33152614+clempetit@users.noreply.github.com>
2018-05-14 14:21:10 +0200

    Add files via upload

--- LcdImage.java
+++ LcdImage.java
@@ -16,10 +16,10 @@ import ch.epfl.gameboj.Preconditions;
  * represents a GameBoy image.
  */
 public final class LcdImage {
-    
+
     private final int width, height;
     private final List<LcdImageLine> lineList;
-    
+
     /**
      * initializes the width and the height of the image and the list of its
      * lines.
@@ -35,12 +35,13 @@ public final class LcdImage {
      *             if lineList is empty or the height is invalid
      */
     public LcdImage(int width, int height, List<LcdImageLine> lineList) {
-        Preconditions.checkArgument(!(lineList.isEmpty()) && (height == lineList.size()));
+        Preconditions.checkArgument(
+                !(lineList.isEmpty()) && (height == lineList.size()));
         this.width = width;
         this.height = height;
         this.lineList = Collections.unmodifiableList(new ArrayList<>(lineList));
     }
-    
+
     /**
      * returns the width of the image.
      * 
@@ -49,7 +50,7 @@ public final class LcdImage {
     public int width() {
         return width;
     }
-    
+
     /**
      * returns the height of the image.
      * 
@@ -58,7 +59,7 @@ public final class LcdImage {
     public int height() {
         return height;
     }
-    
+
     /**
      * gets, in the form of an integer included between 0 and 3, the color of
      * the pixel of given index (x,y).
@@ -91,7 +92,7 @@ public final class LcdImage {
             }
         }
     }
-    
+
     /**
      * checks if the given object is an LcdImage and if its image line list is
      * equal to this image line list.
@@ -105,9 +106,9 @@ public final class LcdImage {
         if (!(that instanceof LcdImage)) {
             return false;
         }
-        return this.lineList.equals(((LcdImage)that).lineList);
+        return this.lineList.equals(((LcdImage) that).lineList);
     }
-    
+
     /**
      * returns the hashcode of the list of the image lines .
      * 
@@ -116,15 +117,15 @@ public final class LcdImage {
     public int hashcode() {
         return Objects.hash(lineList);
     }
-    
+
     /**
      * represents an LcdImage builder.
      */
     public final static class Builder {
-        
+
         private int width, height;
         private List<LcdImageLine> lineList;
-        
+
         /**
          * initializes the width and the height of the image to build that is
          * initially empty, that is that all its pixels have the color 0.
@@ -140,9 +141,10 @@ public final class LcdImage {
             Preconditions.checkArgument(width >= 0 && height >= 0);
             this.width = width;
             this.height = height;
-            lineList = new ArrayList<>(Collections.nCopies(height, new LcdImageLine.Builder(width).build()));
+            lineList = new ArrayList<>(Collections.nCopies(height,
+                    new LcdImageLine.Builder(width).build()));
         }
-        
+
         /**
          * sets the line of given index to the given value.
          * 
@@ -161,7 +163,7 @@ public final class LcdImage {
             lineList.set(index, newValue);
             return this;
         }
-        
+
         /**
          * returns the image built from that builder.
          * 
