35a2df073fc6ac9c71dd273dd7a80e1fc1e22ea4
yanisepfl <33152568+yanisepfl@users.noreply.github.com>
2018-05-14 14:42:20 +0200

    Update ImageConverter.java

--- ImageConverter.java
+++ ImageConverter.java
@@ -10,11 +10,19 @@ import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.image.PixelWriter;
 
+/**
+ * represents a converter of Game Boy images into JavaFX images.
+ */
 public final class ImageConverter {
     
-    private static final int[] COLOR_MAP = new int[] { 0xFF_FF_FF_FF, 0xDD_D3_D3_D3,
-            0xFF_A9_A9_A9, 0xFF_00_00_00 };                                                   // ???
+    private static final int[] COLOR_MAP = new int[] { 0xFF_FF_FF_FF, 0xFF_D3_D3_D3,
+            0xFF_A9_A9_A9, 0xFF_00_00_00 };                                                   
     
+    /**
+     * converts the given LcdImage to a JavaFX image.
+     * @param li the LcdImage
+     * @return a JavaFX image
+     */
     public static Image convert(LcdImage li) {
         
         WritableImage wi = new WritableImage(li.width(), li.height());
