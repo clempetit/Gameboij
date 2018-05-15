47a42d10a3331c7e1ae3bc26590db9a6cc6fcf2c
clempetit <33152614+clempetit@users.noreply.github.com>
2018-05-15 04:17:06 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,20 +5,14 @@
 
 package ch.epfl.gameboj.gui;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.imageio.ImageIO;
 
 import ch.epfl.gameboj.GameBoy;
 import ch.epfl.gameboj.component.Joypad.Key;
 import ch.epfl.gameboj.component.cartridge.Cartridge;
 import ch.epfl.gameboj.component.lcd.LcdController;
-import ch.epfl.gameboj.component.lcd.LcdImage;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -29,27 +23,28 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+
 /**
  * contains the simulator's main program.
  */
-public final class Main extends Application{
-    
-    private static Map<KeyCode, Key> keys = new HashMap<>();
+public final class Main extends Application {
+
+    private static Map<KeyCode, Key> keys = Map.of(
+            KeyCode.D, Key.RIGHT,
+            KeyCode.A, Key.LEFT,
+            KeyCode.W, Key.UP,
+            KeyCode.S, Key.DOWN,
+            KeyCode.O, Key.A,
+            KeyCode.K, Key.B,
+            KeyCode.B, Key.SELECT,
+            KeyCode.N, Key.START
+            );
 
     /**
-     * calls the method launch of Application with the given arguments.
+     * calls the method launch of Application with the given argument.
      * @param args the arguments
      */
     public static void main(String[] args) {
-        keys.put(KeyCode.D, Key.RIGHT);                 // Faire comme ça pour remplir la HashMap ?
-        keys.put(KeyCode.A, Key.LEFT);
-        keys.put(KeyCode.W, Key.UP);
-        keys.put(KeyCode.S, Key.DOWN);
-        keys.put(KeyCode.O, Key.A);
-        keys.put(KeyCode.K, Key.B);
-        keys.put(KeyCode.B, Key.SELECT);
-        keys.put(KeyCode.N, Key.START);
-
         Application.launch(args);
     }
 
@@ -61,15 +56,15 @@ public final class Main extends Application{
         if (argList.size() != 1)
             System.exit(1);
 
-        File romFile = new File("LegendOfZelda.gb");
-        // argList.get(0));
+        File romFile = new File("SuperMarioLand.gb");
+                //argList.get(0));
         GameBoy gb = new GameBoy(Cartridge.ofFile(romFile));
 
         ImageView imageView = new ImageView();
         imageView.setFitWidth(LcdController.LCD_WIDTH * 2);
-        imageView.setFitHeight(LcdController.LCD_HEIGHT * 2);
+        imageView.setFitHeight(LcdController.LCD_HEIGHT * 2);   // comme ça ?
 
-        Image image = ImageConverter.convert(gb.lcdController().currentImage());
+        Image image = ImageConverter.convert(gb.lcdController().currentImage()); // nécessaire de le mettre ici aussi ?
         imageView.setImage(image);
 
         BorderPane borderPane = new BorderPane();
@@ -81,9 +76,6 @@ public final class Main extends Application{
             public void handle(KeyEvent event) {
                 if (keys.get(event.getCode()) != null)
                     gb.joypad().keyPressed(keys.get(event.getCode()));
-                else if (event.getCode() == KeyCode.C) {                // Extension Capture d'écran
-                    screenshot(gb); //
-                }
             }
         });
 
@@ -104,7 +96,7 @@ public final class Main extends Application{
             @Override
             public void handle(long now) {
                 gb.runUntil(
-                        (long) ((now - start) * GameBoy.CYCLES_PER_NANOSECOND)); // procéder comme ça ?
+                        (long) ((now - start) * GameBoy.CYCLES_PER_NANOSECOND));
                 Image image = ImageConverter
                         .convert(gb.lcdController().currentImage());
                 imageView.setImage(image);
@@ -112,20 +104,5 @@ public final class Main extends Application{
         };
         timer.start();
     }
-
-    private static void screenshot(GameBoy gb) {
-        int[] COLOR_MAP = new int[] { 0xFF_FF_FF, 0xD3_D3_D3, 0xA9_A9_A9,
-                0x00_00_00 };
-        try {
-            LcdImage li = gb.lcdController().currentImage();
-            BufferedImage i = new BufferedImage(li.width(), li.height(),
-                    BufferedImage.TYPE_INT_RGB);
-            for (int y = 0; y < li.height(); ++y)
-                for (int x = 0; x < li.width(); ++x)
-                    i.setRGB(x, y, COLOR_MAP[li.get(x, y)]);
-            ImageIO.write(i, "png", new File("gb.png"));
-        } catch (IOException e) {
-            System.out.println("Screenshot could not be saved");
-        }
-    }
+    
 }
